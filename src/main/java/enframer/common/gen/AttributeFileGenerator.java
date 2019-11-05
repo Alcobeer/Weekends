f200da58c486605930ed2d3124a08449b32b8acc
Pastyxov <Mat_pas2xov@mail.ru>
2019-11-05 20:36:44 +0300

    Усовершенствованна система бафов за изменение уровней

--- src/main/java/enframer/common/gen/AttributeFileGenerator.java
+++ src/main/java/enframer/common/gen/AttributeFileGenerator.java
@@ -67,12 +67,12 @@ public class AttributeFileGenerator implements IFileGenerator {
                 int initialValue = attribute.valueProperty().get();
 
 
-                int valueOut=initialValue;
-                for(int i = 0; i < rarityCategory.getRarityFactor() ; i++){
+                int valueOut = initialValue;
 
+                for (int i = 0; i <= rarityCategory.getRarityFactor() - (level == 1 ? 1 : 0); i++) {
                     valueOut += Math.ceil(initialValue / 100F * rarityIn) * level;
+                }
 
-              }
 
 
                 b.append("   ").append(attribute.nameProperty().get()).append(": +").append(valueOut).append("\n");
